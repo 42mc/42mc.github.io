9974597cf14183a12eec55fc8113c9ab33115241
42willow <70784752+42Willow@users.noreply.github.com>
2023-06-10 17:25:59 +1000

    Widget. I'm begging you, don't look at the css .-.

--- script.js
+++ script.js
@@ -1,3 +1,37 @@
+// let mainFlexHeight = document.querySelector(".scroll-section").offsetTop;
+// document.documentElement.style.setProperty('--offset-height', mainFlexHeight + "px");
+// console.log("mainFlexHeight: " + mainFlexHeight + "px");
+// document.documentElement.style.setProperty('--main-height', document.querySelector(".content").offsetHeight + "px");
+// size of main-flex
+
+const ip = "random-name-coming-soon-tm.ply.gg";
+// const ip = "play.hypixel.net";
+
+var url = `https://api.minetools.eu/ping/${ip}/25565`;
+ 
+$.getJSON(url, function(r) {
+    //data is the JSON string
+ if(r.error){
+    $('#rest').html('Server Offline');
+   return false;
+ } 
+var pl = '';
+ if(r.players.sample.length > 0 ){ pl = '<br>OP: '+r.players.sample[0].name;  } 
+  $('#rest').html(r.description.replace(/§(.+?)/gi, '')+'<br><b>Players Online:</b> '+r.players.online+pl);
+//  $('#favicon').attr('src', r.favicon);
+    
+});
+
+
+
+// on page load
+window.addEventListener("load", function() {
+    let mainFlexHeight = document.querySelector(".main-flex").offsetHeight;
+    document.documentElement.style.setProperty('--main-flex-height', mainFlexHeight + "px");
+    console.log("mainFlexHeight: " + mainFlexHeight + "px");
+});
+
+
 // Listener for copy-ip-btn click
 document.querySelector(".copy-ip-btn").addEventListener("click", function() {
     // Get the text field
